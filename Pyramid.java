a4326f18775081d6a0df5481df428f38d4d5fe44
Kancharla Vaishnavi <159788214+vaishnavi1278@users.noreply.github.com>
2024-07-08 13:27:52 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -2,11 +2,11 @@ class Pyramid{
 public static void main(String args[])
 {
 int n=Integer.parseInt(args[0]);
-for(int i=0;i<n;i++)
+for(int i=0;i<n;i++){
 for(int j=0;j<i;j++){
 System.out.print(i+"\t");
 }
 System.out.println("\n");
 }
 }
-
+}
