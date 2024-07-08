6963935239df1590ac63c6801f299233f26161e7
Kancharla Vaishnavi <159788214+vaishnavi1278@users.noreply.github.com>
2024-07-08 13:19:42 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,12 @@
+class Pyramid{
+public static void main(String args[])
+{
+int n=Integer.parseInt(args[0]);
+int i,j;
+for(int i=0;i<n;i++)
+for(int j=0;j<i;j++)
+System.out.print(i+"\t");
+System.out.println();
+}
+}
+
